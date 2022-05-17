914cd2791cc32a6d75b99b5338839d9512bb255d
addehd <hej@adolfo.se>
2022-05-18 00:15:00 +0200

    Pass max-with to galley component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,13 +4,13 @@ import Gallery from "../components/gallery.svelte"
 import junio from '$lib/junio.webp'
 
 let paintings = [
-  { name: 'Junio', img: junio },
+  { name: 'Junio', img: junio, style: 'max-width: 40rem;' },
   { name: 'Selva', img: junio },
   { name: 'Otono', img: junio }, 
 ]
 </script>
 
 <Header/>
-{#each paintings as { name, img }, i}
-  <Gallery imgg={img} name={name}/>
+{#each paintings as { name, img, style }, i}
+  <Gallery imgg={img} name={ name } style={ style } />
 {/each}
